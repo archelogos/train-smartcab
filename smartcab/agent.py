dbffe06e20ca6a441acd4e1d7fc17c7e1c2d0200
Sergio Gordillo <sgordillogallardo@gmail.com>
2016-06-14 19:52:44 +0200

    Understanding and StepOne
    + Random actions for the Agent

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -23,9 +23,10 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        
+
         # TODO: Select action according to your policy
-        action = None
+        # STEP_ONE: Random action
+        action = random.choice([None, 'forward', 'left', 'right'])
 
         # Execute action and get reward
         reward = self.env.act(self, action)
@@ -41,8 +42,9 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
+    # TODO: Change later enforce_deadline=True
 
     # Now simulate it
     sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
