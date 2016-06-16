cceec2c8618fa28531fd3e749f5f0289da294a76
Sergio Gordillo <sgordillogallardo@gmail.com>
2016-06-16 12:06:11 +0200

    Implementing Q-Learning algorithm

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -11,6 +11,17 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        # Valid actions
+        self.actions = [None, 'forward', 'left', 'right']
+
+        # Q-Learning
+        # Alpha (learning rate)
+        self.alpha = 0.5
+        # Gamma (discount factor)
+        self.gamma = 0.5
+        self.epsilon = 0
+        self.Q = {}
+        self.Q_default_value = 0
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -27,22 +38,70 @@ class LearningAgent(Agent):
         # Is deadline input necessary?
         self.state = (
             ('next', self.next_waypoint),
-            ('light', inputs['light']),
-            ('oncoming', inputs['oncoming']),
-            ('left', inputs['left']),
-            ('right', inputs['right']),
-            ('deadline', deadline))
+            ('light', inputs['light']))
+            # ('oncoming', inputs['oncoming']),
+            # ('left', inputs['left']),
+            # ('right', inputs['right']))
+            # ('deadline', deadline))
+
+        state = self.state
+        print "LearningAgent.update(): state_zero = {}".format(state)  # [debug]
 
         # TODO: Select action according to your policy
         # STEP_ONE: Random action
-        action = random.choice([None, 'forward', 'left', 'right'])
+        # Exploring -> random
+        # Best choice -> agent experience Q matrix
+        action = random.choice(actions)
+
+        print "LearningAgent.update(): action = {}".format(action)  # [debug]
+
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
+        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+        inputs = self.env.sense(self)
+        deadline = self.env.get_deadline(self)
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        state_prime = (
+            ('next', self.next_waypoint),
+            ('light', inputs['light']))
+            # ('oncoming', inputs['oncoming']),
+            # ('left', inputs['left']),
+            # ('right', inputs['right']))
+            # ('deadline', deadline))
+        print "LearningAgent.update(): state_one = {}".format(state_prime)  # [debug]
+
+        self.update_Q(state, action, reward, state_prime)
+
+        print "------------------------------------------------------------------"  # [debug]
+        #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+
+    def get_max_Q_for_state(self, state):
+        max_q = []
+
+        for action in self.actions:
+            if (state, action) not in self.Q:
+                return 0
+            else:
+                max_q.append(self.Q[(state, action)])
+
+        return max(max_q)
+
+
+    def update_Q(self, state, action, reward, state_prime):
+        print "Updating Q MATRIX"
+
+        # Q(s,a) = (1- alpha)*Q(s,a) + alpha*(reward + gamma * max_Q(s', a'))
+
+        # Init value if it doesn't exist: Q(self.state, self.action) = 0
+        if (state, action) not in self.Q:
+                self.Q[(state, action)] = self.Q_default_value
+
+        self.Q[(state, action)] = (1 - self.alpha) * self.Q[(state, action)] + \
+            self.alpha * (reward + self.gamma * \
+                self.get_max_Q_for_state(self.state_prime))
 
 
 def run():
@@ -56,7 +115,7 @@ def run():
     # TODO: Change later enforce_deadline=True
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=2, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
